b6446096b5ced3974ac56711fcf6f56ec13b7ff3
Oguzhan NAMLICI <oguzhannamlici@gmail.com>
2022-11-26 16:44:59 +0300

    do-While Loop

---
+++ Java/example7/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int i = 1;
+        do {
+            for(int j = 1; j <= 10; j++){
+                for(int k = 1; k <= 10; k++){
+                    System.out.println(j + " * " + k + " = " + (j*k));
+                }
+                System.out.println();
+                i++;
+            }
+        }while (i <= 10);
+    }
+}
